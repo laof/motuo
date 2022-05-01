7e7c71515a36258db760beb5597f30eb2a2c3730
laof <345603828@qq.com>
2022-05-01 18:51:19 +0800

    update release confi

--- cd.dart
+++ cd.dart
@@ -3,17 +3,13 @@ import 'dart:io';
 void main(List<String> args) {
   var pf = args[0];
   print("build planform:" + pf);
-  if (pf == "apk") {
-    File apk = File('build/app/outputs/flutter-apk/app-release.apk');
 
-    var f = File("motuo-apk.apk");
-    if (f.existsSync()) {
-      f.deleteSync();
-    }
+  File apk = File('build/app/outputs/flutter-apk/app-release.apk');
 
-    apk.copySync("motuo-apk.apk");
-  } else {
-    // File app = File('build/ios/iphoneos/Runner.app.');
-    // app.copySync("motuo-app.app");
+  var f = File("motuo-apk.apk");
+  if (f.existsSync()) {
+    f.deleteSync();
   }
+
+  apk.copySync("motuo-apk.apk");
 }
--- cicd.bat
+++
@@ -1,6 +0,0 @@
-set app=ci-motuo.apk
-flutter build apk &&^
-copy build\app\outputs\apk\release\app-release.apk .\ &&^
-del/f/s/q %app%  &&^
-rename app-release.apk %app% &&^
-echo ==== build successfully %app% =====
