e803f2b04f839569a3c27b932c2e49f4de1c9410
laof <345603828@qq.com>
2022-05-01 08:32:49 +0000

    added motuo-app.app

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/hostedtoolcache/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/runner/work/motuo/motuo"
+export "FLUTTER_ROOT=/Users/runner/hostedtoolcache/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/runner/work/motuo/motuo"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -9,5 +9,5 @@ export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
-export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/runner/work/motuo/motuo/.dart_tool/package_config.json"
