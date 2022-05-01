7d9541885d67e940f22ce6fbfdae69c8c9cefefe
laof <345603828@qq.com>
2022-05-01 15:17:28 +0800

    update yaml

--- cd.dart
+++ cd.dart
@@ -5,6 +5,12 @@ void main(List<String> args) {
   print("build planform:" + pf);
   if (pf == "apk") {
     File apk = File('build/app/outputs/flutter-apk/app-release.apk');
+
+    var f = File("motuo-apk.apk");
+    if (f.existsSync()) {
+      f.deleteSync();
+    }
+
     apk.copySync("motuo-apk.apk");
   } else {
     File app = File('build/ios/iphoneos/Runner.app.');
