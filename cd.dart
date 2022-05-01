4a969b2e94821edaedf4d0b233f880ba1a435cdf
laof <345603828@qq.com>
2022-05-01 15:15:24 +0800

    update yml

--- cd.dart
+++ cd.dart
@@ -7,7 +7,7 @@ void main(List<String> args) {
     File apk = File('build/app/outputs/flutter-apk/app-release.apk');
     apk.copySync("motuo-apk.apk");
   } else {
-    File app = File('build/ios/iphoneos/Runner.app');
+    File app = File('build/ios/iphoneos/Runner.app.');
     app.copySync("motuo-app.app");
   }
 }
