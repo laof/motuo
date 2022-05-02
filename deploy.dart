ae04caecc2a8fffd5ca56ceec3cddb942a3e5579
laof <345603828@qq.com>
2022-05-02 09:23:50 +0800

    build:test

--- deploy.dart
+++ deploy.dart
@@ -8,6 +8,6 @@ void main(List<String> args) {
   var pf = args[0];
   print("build folder pipeline: " + pf);
   File apk = File('build/app/outputs/flutter-apk/app-release.apk');
-  new Directory("app").createSync();
-  apk.copySync("app/motuo.apk");
+  new Directory("output").createSync();
+  apk.copySync("output/motuo.apk");
 }
