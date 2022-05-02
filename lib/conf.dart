5c97f98e8e6ccf39e40d34018428eaae5289adec
laof <345603828@qq.com>
2022-05-02 23:28:50 +0800

    cicd: update conf.json

--- lib/conf.dart
+++ lib/conf.dart
@@ -1,4 +1,4 @@
 var server = "https://laof.github.io";
-var config = server + "/motuo/conf.json";
+var config = server + "/motuo/sys.json";
 var oktxt = server + "/ok.txt";
 var okstr = "D6A04790-A371-4357-81B7-C7290A392A3A";
